fc486dc2082da92527269e639cfb1c04596a2819
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2023-10-16 03:07:37 -0700

    1st commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import keypad from './keypad';
+
+function App() {
+  return (
+    <div className="App">
+   <h2>Calculator</h2>
+   <keypad/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/keypad.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import React  from "react";
+
+function keypad(){
+
+
+    return (
+        <div className="keypad">
+chec check
+        </div>
+
+    );
+};
+export default keypad;
